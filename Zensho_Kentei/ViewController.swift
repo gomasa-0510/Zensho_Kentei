9582f36a878712adfd8a7140cec475174d3bfaf9
後藤昌也 <mas_goto@gotoumasaya-no-macbook-pro.local>
2018-01-22 15:06:14 +0900

    Initial Commit

---
+++ Zensho_Kentei/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zensho_Kentei
+//
+//  Created by 後藤昌也 on 2018/01/22.
+//  Copyright © 2018年 Masaya Goto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Zensho_KenteiTests/Zensho_KenteiTests.swift
@@ -0,0 +1,36 @@
+//
+//  Zensho_KenteiTests.swift
+//  Zensho_KenteiTests
+//
+//  Created by 後藤昌也 on 2018/01/22.
+//  Copyright © 2018年 Masaya Goto. All rights reserved.
+//
+
+import XCTest
+@testable import Zensho_Kentei
+
+class Zensho_KenteiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
